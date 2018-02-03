22ea87a2fef32bc231b2d2679efc1fd36c441e49
hy_wg <wg@ihuyue.cn>
2018-02-03 18:11:27 +0800

    redis

--- src/king/main.go
+++ src/king/main.go
@@ -3,17 +3,24 @@ package king
 import (
 	"github.com/gin-gonic/gin"
 	"db"
+	"redis"
 )
 
 func Main() {
 	//初始化DB资源
 	db.InitDb()
 	defer db.DevContext.Db.Close()
+
+	//初始化Redis资源
+	redis.InitRedisConn()
+	defer redis.DevRedeisConn.Conn.Close()
 	router := gin.Default()
 	router.GET("/king/add", Add)
 	router.GET("/king/delete", Delete)
 	router.GET("/king/find", Find)
 	router.GET("/king/find_all", FindAll)
 	router.GET("/king/update", Update)
+	router.GET("/king/set", Set)
+	router.GET("/king/get", Get)
 	router.Run(":8010")
 }
--- src/router/main.go
+++ src/router/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"king"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	king.Main()
 }
